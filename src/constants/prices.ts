177e43344f0e34f2f15a9c4bd46e3592b767884a
Bruno <bruno@MacBook-Pro-de-Bruno.local>
2024-09-29 22:05:34 -0300

    fixing filter pricing ---

--- src/constants/prices.ts
+++ src/constants/prices.ts
@@ -1,49 +1,9 @@
-export const minPrice = [
-  { id: 1, title: "R$ 10.000", value: 10000 },
-  { id: 2, title: "R$ 20.000", value: 20000 },
-  { id: 3, title: "R$ 30.000", value: 30000 },
-  { id: 4, title: "R$ 40.000", value: 40000 },
-  { id: 5, title: "R$ 50.000", value: 50000 },
-  { id: 6, title: "R$ 60.000", value: 60000 },
-  { id: 7, title: "R$ 70.000", value: 70000 },
-  { id: 8, title: "R$ 80.000", value: 80000 },
-  { id: 9, title: "R$ 90.000", value: 90000 },
-  { id: 10, title: "R$ 100.000", value: 100000 },
-  { id: 11, title: "R$ 110.000", value: 110000 },
-  { id: 12, title: "R$ 120.000", value: 120000 },
-  { id: 13, title: "R$ 130.000", value: 130000 },
-  { id: 14, title: "R$ 140.000", value: 140000 },
-  { id: 15, title: "R$ 150.000", value: 150000 },
-  { id: 16, title: "R$ 160.000", value: 160000 },
-  { id: 17, title: "R$ 170.000", value: 170000 },
-  { id: 18, title: "R$ 180.000", value: 180000 },
-  { id: 19, title: "R$ 190.000", value: 190000 },
-  { id: 20, title: "R$ 200.000", value: 200000 },
-  { id: 21, title: "R$ 210.000", value: 210000 },
-  { id: 22, title: "R$ 220.000", value: 220000 },
-];
+export const minPrice = Array.from({ length: 181 }, (_, index) => {
+  const value = index * 5000;
+  return { id: index + 1, title: `R$ ${value.toLocaleString()}`, value };
+});
 
-export const maxPrice = [
-  { id: 1, title: "R$ 10.000", value: 10000 },
-  { id: 2, title: "R$ 20.000", value: 20000 },
-  { id: 3, title: "R$ 30.000", value: 30000 },
-  { id: 4, title: "R$ 40.000", value: 40000 },
-  { id: 5, title: "R$ 50.000", value: 50000 },
-  { id: 6, title: "R$ 60.000", value: 60000 },
-  { id: 7, title: "R$ 70.000", value: 70000 },
-  { id: 8, title: "R$ 80.000", value: 80000 },
-  { id: 9, title: "R$ 90.000", value: 90000 },
-  { id: 10, title: "R$ 100.000", value: 100000 },
-  { id: 11, title: "R$ 110.000", value: 110000 },
-  { id: 12, title: "R$ 120.000", value: 120000 },
-  { id: 13, title: "R$ 130.000", value: 130000 },
-  { id: 14, title: "R$ 140.000", value: 140000 },
-  { id: 15, title: "R$ 150.000", value: 150000 },
-  { id: 16, title: "R$ 160.000", value: 160000 },
-  { id: 17, title: "R$ 170.000", value: 170000 },
-  { id: 18, title: "R$ 180.000", value: 180000 },
-  { id: 19, title: "R$ 190.000", value: 190000 },
-  { id: 20, title: "R$ 200.000", value: 200000 },
-  { id: 21, title: "R$ 210.000", value: 210000 },
-  { id: 22, title: "R$ 220.000", value: 220000 },
-];
+export const maxPrice = Array.from({ length: 181 }, (_, index) => {
+  const value = index * 5000;
+  return { id: index + 1, title: `R$ ${value.toLocaleString()}`, value };
+});
