c96ae037206af026a1b1653c3ccc7e98940a8e89
Bruno <bruno@MacBook-Pro-de-Bruno.local>
2024-10-31 13:57:00 -0300

    choice accessories car and new components

--- src/components/filters/documents-late-filter.tsx
+++ src/components/filters/documents-late-filter.tsx
@@ -1,43 +1,45 @@
 "use client";
+
 import React from "react";
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
-} from "@/components/ui/select"; // Importa o Select do ShadCN UI
+import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "../ui/label";
 
 interface DocumentsLateFilterProps {
-  isLate: string; // Mantém como string para uso com o Select
-  onChange: (isLate: boolean) => void;
-  onSearch: (isLateBoolean: boolean) => void; // Adiciona uma função para realizar a busca
+  isLate: boolean | undefined;
+  onChange: (isLate: boolean | undefined) => void;
 }
 
 const DocumentsLateFilter: React.FC<DocumentsLateFilterProps> = ({
   isLate,
   onChange,
-  onSearch,
 }) => {
-  const handleSelectChange = (value: string) => {
-    const isLateBoolean = value === "sim";
-    onChange(isLateBoolean);
-    onSearch(isLateBoolean); // Realiza a busca com o valor booleano convertido
+  const handleCheckboxChange = (value: boolean) => {
+    onChange(value);
   };
 
   return (
     <div>
       <Label htmlFor="documentsLate">Documentos Atrasados</Label>
-      <Select onValueChange={handleSelectChange} value={isLate}>
-        <SelectTrigger id="documentsLate" className="mt-2">
-          <SelectValue placeholder="Selecione uma opção" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="sim">Sim</SelectItem>
-          <SelectItem value="não">Não</SelectItem>
-        </SelectContent>
-      </Select>
+      <div className="flex items-center space-x-2">
+        {/* Opção "Sim" */}
+        <div className="flex items-center space-x-2">
+          <Checkbox
+            id="documentsLateYes"
+            checked={isLate === true}
+            onCheckedChange={() => handleCheckboxChange(true)}
+          />
+          <label htmlFor="documentsLateYes">Sim</label>
+        </div>
+        {/* Opção "Não" */}
+        <div className="flex items-center space-x-2">
+          <Checkbox
+            id="documentsLateNo"
+            checked={isLate === false}
+            onCheckedChange={() => handleCheckboxChange(false)}
+          />
+          <label htmlFor="documentsLateNo">Não</label>
+        </div>
+      </div>
     </div>
   );
 };
